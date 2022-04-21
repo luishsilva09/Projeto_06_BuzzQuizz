e155fb5ef324c234232859cf3f848295319df8a5
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-04-21 19:59:49 -0300

    adiciona coleta e verificacao de dados de perguntas e respostas registradas

--- js/script-pag3.js
+++ js/script-pag3.js
@@ -1,4 +1,11 @@
 let imagemInvalida;
+let qtdPerguntas;
+// funcoes de estilo
+
+function selecionarPerguntaCadastro(containerPergunta) {
+    document.querySelector(".cadastro-perguntas .selecionada").classList.remove("selecionada");
+    containerPergunta.classList.add("selecionada");
+}
 
 // funcoes de redirecionamento entre páginas
 
@@ -24,54 +31,162 @@ function direcionarCadastroParaHome() {
 }
 
 // manipulacao da entrada de informacoes dadas pelo usuario
-function validarUrlImagem(url) {
+function carregarUrlImagem(url) {
     const img = new Image();
     img.src = url;
-
+    
     img.onload = function () {
         imagemInvalida = false;
     }
-
+    
     img.onerror = function () {
         imagemInvalida = true;
     }
 }
 
-function pegarInformacoesIniciais() {
-    const tituloQuiz = document.querySelector(".informacoes-iniciais .titulo").value;
+function adicionaCadastroPerguntasHTML() {
+
+    document.querySelector(".cadastro-quizz .cadastro-perguntas").innerHTML = "";
+
+    for(let i = 0; i < qtdPerguntas; i++) {
+        document.querySelector(".cadastro-quizz .cadastro-perguntas").innerHTML += `
+        <div class="cadastro-pergunta" onclick="selecionarPerguntaCadastro(this)">
+            <div class="topo-pergunta">
+                <h4>Pergunta ${i + 1}</h4>
+                <img src="./imagens/Vector.svg" alt="icone-papel-lapis">
+            </div>
+            <div class="perguntas-respostas">
+                <input type="text" class="pergunta" placeholder="Texto da pergunta">
+                <input type="text" class= "cor-pergunta" placeholder="Cor de fundo da pergunta">
+
+                <h4>Resposta Correta</h4>
+                <input type="text" class="resposta-certa" placeholder="Texto da resposta">
+                <input type="text" class="imagem-resposta-certa" placeholder="URL da imagem">
+
+                <h4>Respostas Incorretas</h4>
+                <div class="resposta-incorreta">
+                    <input type="text" placeholder="Resposta incorreta 1">
+                    <input type="text" placeholder="URL da imagem 1">
+                </div>
+                <div class="resposta-incorreta">
+                    <input type="text" placeholder="Resposta incorreta 2">
+                    <input type="text" placeholder="URL da imagem 2">
+                </div>
+                <div class="resposta-incorreta">
+                    <input type="text" placeholder="Resposta incorreta 3">
+                    <input type="text" placeholder="URL da imagem 3">
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    document.querySelector(".cadastro-perguntas .cadastro-pergunta:first-child").classList.add("selecionada");
+    document.querySelector(".cadastro-perguntas").innerHTML += `
+        <button onclick="coletarDadosPerguntas()">Prosseguir para criar níveis</button>
+        `
+}
+
+function coletarDadosIniciais() {
+    const tituloQuiz = document.querySelector(".informacoes-iniciais .titulo").value.split(" ").filter(palavra => palavra !== "");
+    const tituloQuizFormatado = tituloQuiz.join(" ");
     const urlImagem = document.querySelector(".informacoes-iniciais .url-img").value;
-    const qtdPerguntas = Number(document.querySelector(".informacoes-iniciais .qtd-perguntas").value);
+    qtdPerguntas = Number(document.querySelector(".informacoes-iniciais .qtd-perguntas").value);
     const qtdNiveis = Number(document.querySelector(".informacoes-iniciais .qtd-niveis").value);
 
-    validarUrlImagem(urlImagem);
+    carregarUrlImagem(urlImagem);
 
-    const promisseImagem = axios.get(urlImagem);
+    setTimeout(function () {
 
-    promisseImagem.then(
-        setTimeout(
+        if(tituloQuizFormatado.length < 20 || tituloQuizFormatado.length > 65) {
+            alert("O título do quizz deve ter entre 20 e 65 caracateres");
+        } else if(imagemInvalida) {
+            alert("A URL da imagem é inválida");
+            document.querySelector(".informacoes-iniciais .url-img").value = "";
+        } else if(qtdPerguntas < 3 || isNaN(qtdPerguntas)) {
+            alert("A quantidade de perguntas deve ser maior ou igual a 3")
+            document.querySelector(".informacoes-iniciais .qtd-perguntas").value = "";
+        } else if(qtdNiveis < 2 || isNaN(qtdNiveis)) {
+            alert("A quantidade de níveis deve ser maior ou igual a 2");
+            document.querySelector(".informacoes-iniciais .qtd-niveis").value = "";
+        } else {
+            document.querySelector(".informacoes-iniciais .titulo").value = "";
+            document.querySelector(".informacoes-iniciais .url-img").value = "";
+            document.querySelector(".informacoes-iniciais .qtd-perguntas").value = "";
+            document.querySelector(".informacoes-iniciais .qtd-niveis").value = "";
 
-            function () {
-                console.log("Bete")
-            if(tituloQuiz.length < 20 || tituloQuiz.length > 65) {
-                alert("O título do quizz deve ter entre 20 e 65 caracateres");
-            } else if(imagemInvalida) {
-                alert("A URL da imagem é inválida");
-                document.querySelector(".informacoes-iniciais .url-img").value = "";
-            } else if(qtdPerguntas < 3 || isNaN(qtdPerguntas)) {
-                alert("A quantidade de perguntas deve ser maior ou igual a 3")
-                document.querySelector(".informacoes-iniciais .qtd-perguntas").value = "";
-            } else if(qtdNiveis < 2 || isNaN(qtdNiveis)) {
-                alert("A quantidade de níveis deve ser maior ou igual a 2");
-                document.querySelector(".informacoes-iniciais .qtd-niveis").value = "";
-            } else {
-                document.querySelector(".informacoes-iniciais .titulo").value = "";
-                document.querySelector(".informacoes-iniciais .url-img").value = "";
-                document.querySelector(".informacoes-iniciais .qtd-perguntas").value = "";
-                document.querySelector(".informacoes-iniciais .qtd-niveis").value = "";
+            adicionaCadastroPerguntasHTML();
+            direcionarCadastroPerguntas();
+        }
+    }, 400);
+}
 
-                direcionarCadastroPerguntas();
+function coletarDadosPerguntas() {
+    const perguntas = document.querySelectorAll(".cadastro-quizz .cadastro-pergunta");
+    let preenchidoCorretamente;
+
+    for(let i = 0; i < qtdPerguntas; i++) {
+        setTimeout(function () {
+            preenchidoCorretamente = false;
+            const containerPerguntasRespostas = perguntas[i].querySelector(".perguntas-respostas");
+    
+            const textoPergunta = containerPerguntasRespostas.querySelector(".pergunta").value.split(" ").filter(palavra => palavra !== "");
+            const textoPerguntaFormatado = textoPergunta.join(" ");
+            const corPergunta = containerPerguntasRespostas.querySelector(".cor-pergunta").value;
+        
+            const repostaCerta = containerPerguntasRespostas.querySelector(".resposta-certa").value;
+            const imgRepostaCerta = containerPerguntasRespostas.querySelector(".imagem-resposta-certa").value;
+            
+            const respostasErradas = containerPerguntasRespostas.querySelectorAll(".resposta-incorreta :first-child");
+            const imgRespostasErradas = containerPerguntasRespostas.querySelectorAll(".resposta-incorreta :last-child");
+            
+            const veificaCorHexadecimal = texto => /^#[0-9A-F]{6}$/i.test(texto);
+            
+            carregarUrlImagem(imgRepostaCerta);
+            
+            let qtdRespostasVazias = 0;
+            
+            for(let i = 0; i < imgRespostasErradas.length; i++) {
+                if(respostasErradas[i].value.length > 0 && respostasErradas[i].value.trim() !== "") {
+                    carregarUrlImagem(imgRespostasErradas[i].value);
+        
+                } else {
+                    qtdRespostasVazias++;
+                }
             }
-        }, 400)
-    );
+            // colocar um clearTimeout para parar execuções em fila
+            
+            
+            setTimeout(function () {
+                if(textoPerguntaFormatado.length < 20) {
+                    alert("O texto da pergunta deve ter pelo menos 20 caracteres");
+                    i = qtdPerguntas + 1;
+                } else if(corPergunta.length !== 7 || !(veificaCorHexadecimal(corPergunta))) {
+                    alert("Cor hexadecimal inválida, insira no formato: #AAAAAA e sendo uma cor válida");
+                    i = qtdPerguntas + 1;
+                } else if(repostaCerta.length <= 0) {
+                    alert("A resposta certa é obrigatória");
+                    i = qtdPerguntas + 1;
+                } else if(imagemInvalida) {
+                    alert("Uma ou mais URL's é(são) inválida(s)");
+                    i = qtdPerguntas + 1;
+                } else if(qtdRespostasVazias > 2) {
+                    alert("Deve haver pelo menos uma resposta incorreta");
+                    i = qtdPerguntas + 1;
+                } else {
+                    preenchidoCorretamente = true;
+                }
+    
+            }, 200 * (imgRespostasErradas.length + 1));
+        }, 260 * (qtdPerguntas * 3 + 1));
+    }
+
+    setTimeout(function () {
+        console.log(preenchidoCorretamente);
+        if(preenchidoCorretamente) {
+            direcionarCadastroNiveis();
+
+        }
+    }, 400 * (qtdPerguntas * 3 + 1))
 
 }
