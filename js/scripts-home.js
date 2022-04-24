85a027263c5efd48332519af9f296d90b90384a0
luis <luishsilva09@gmail.com>
2022-04-24 15:16:04 -0300

    redenrizar meus quizz

--- js/scripts-home.js
+++ js/scripts-home.js
@@ -1,6 +1,6 @@
 window.scrollTo(0, 0) // carregar pagina no topo
 
-let meusQuizzId =[1];
+let meusQuizzId = JSON.parse(localStorage.getItem("listaIds"));
 let dados = [];
 // funções de direcionamento
 
@@ -26,7 +26,7 @@ function renderizarQuizz(response) {
             <img src="${dados[i].image}">
             <h3>${dados[i].title}</h3>
         </div>`;
-        
+
     };
     console.log(dados)
     meuQuizz();
@@ -37,34 +37,41 @@ function selecionarQuizz(element) {
     const promise = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${id}`);
     promise.then(abrirQuizz);
 };
-function home(){
+//voltar home clicando na logo  
+function home() {
     window.location.reload();
 };
-function meuQuizz(){
-    if (meusQuizzId != ''){
+//verifica quizz do usuario
+function meuQuizz() {
+    if (meusQuizzId != '') {
         document.querySelector(".conteudo-home .criarQuizz").classList.add("escondido")
         document.querySelector(".conteudo-home .meusQuizz ").classList.remove("escondido")
-        for(let i = 0; i < meusQuizzId.length;i++){
+        for (let i = 0; i < meusQuizzId.length; i++) {
             let meu = meusQuizzId[i]
             const promise = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${meu}`);
             promise.then(renderizarMeusQuizz)
-            
+
         };
-        
+
     };
 };
-function renderizarMeusQuizz(response){
-    let dadosMeu =[];
+//faz a renderização do quiz do usuario e faz sumir do todos
+function renderizarMeusQuizz(response) {
+    let dadosMeu = [];
     dadosMeu = response.data
+
+    for (let i = 0; i < dados.length; i++) {
+        if (dadosMeu.id === dados[i].id) {
+            document.getElementById(`${dadosMeu.id}`).classList.add("escondido")
+        };
+    };
     document.querySelector(".meusQuizz .listaQuizz").innerHTML +=
-    `<div class="quizzBanner" onclick="selecionarQuizz(this)" id="${dadosMeu.id}">
-    <div class="gradiente"></div>
-    <img src="${dadosMeu.image}">
-    <h3>${dadosMeu.title}</h3>
-</div>`;
-    
-    
-    console.log(dadosMeu)
+        `<div class="quizzBanner" onclick="selecionarQuizz(this)" id="${dadosMeu.id}">
+        <div class="gradiente"></div>
+        <img src="${dadosMeu.image}">
+        <h3>${dadosMeu.title}</h3>
+    </div>`;
+
 };
 buscarDados();
 
